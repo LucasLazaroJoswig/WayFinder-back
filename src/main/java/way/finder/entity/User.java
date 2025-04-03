4ad0edd0ae699211e86c71287562bad8a40f17e3
Lucas Lazaro Joswig (ext) <lucas.lazaro.external@eviden.com>
2025-04-03 16:41:09 +0200

    core: commit raiz

---
+++ src/main/java/way/finder/entity/User.java
@@ -0,0 +1,44 @@
+package way.finder.entity;
+
+import java.io.Serializable;
+
+import club.nautico.entidades.Socio;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Builder
+@Entity
+@Table(name = "user")
+public class User implements Serializable{
+	static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+    @NotNull
+    private String nombre;
+    @NotNull
+    private String apellidos;
+    @NotNull
+    private String username;
+    @NotNull
+    @Email
+    private String email;
+    @NotNull
+    private String password;
+	
+}
---
+++ src/test/java/way/finder/WayFinderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package way.finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayFinderBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
